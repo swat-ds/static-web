f7852515723d96f0c2b0b3d8c1f18553ab683b9a
Nabil Kashyap <nkashya1@swarthmore.edu>
2019-02-08 17:58:41 -0500

    updates for workshop

---
+++ assets/scripts/lunr-ui.js
@@ -0,0 +1,77 @@
+$.getJSON("/assets/scripts/lunr-index.json", function(index_json) {
+
+window.index = new elasticlunr.Index;
+window.store = index_json;
+index.saveDocument(false);
+index.setRef('lunr_id');
+index.addField('pid');
+index.addField('title');
+index.addField('location_transcription');
+index.addField('zipcode');
+index.addField('gen_neighborhood');
+// index.addField('outside_phl');
+// index.addField('age');
+// index.addField('age_range');
+// index.addField('instagram');
+// index.addField('image_file_name');
+// index.addField('proposal_transcription');
+index.addField('topic');
+index.addField('type');
+// index.addField('keywords_other');
+// index.addField('credit');
+// index.addField('image_labels');
+// index.addField('twitter');
+// index.addField('facebook');
+
+// add docs
+for (let i in store){index.addDoc(store[i]);}
+
+$('input#search').on('keyup', function() {
+
+var results_div = $('#results');
+var query = $(this).val();
+var results = index.search(query, { boolean: 'AND', expand: true });
+results_div.empty();
+
+console.log(results)
+
+var results_string = '<p><small>Showing ';
+results_string += results.length > 500 ? '500  of ' + results.length : results.length; 
+results_string += " results.</small></p>";
+ 
+results_div.prepend(results_string);
+
+var resultsArray = []
+
+for (let r in results) {
+
+let ref = results[r].ref;
+let item = store[ref];
+
+resultsArray.push(item);
+
+}
+
+for (let r in resultsArray.sort(nameSort)){
+
+let item = resultsArray[r];
+var meta = '';
+
+if (item.age){ meta += '<strong>Age ' + item.age + '. ';}
+if (item.neighborhood){ meta += 'From ' + item.neighborhood.split(',')[0] + '. ';}
+meta += "</strong>";
+if (item.proposal_transcription){ meta += '<br/>' + item.proposal_transcription.slice(0,140) + '... ';}
+
+var result = '<div class="result"><a href="http://proposals.monumentlab.com/monuments/' + item.pid + '"><img class="sq-thumb-sm" src="http://proposals.monumentlab.com/assets/monuments/thumbs/' + item.image_file_name + '"/>&nbsp;&nbsp;&nbsp;<p><span class="title">' + item.title + '.</span><br>' + meta + '</p></a></div>';
+results_div.append(result);
+}
+
+});
+});
+
+
+function nameSort(a,b) {
+    let title_a = a.title.replace(/[\[\].,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase();
+    let title_b = b.title.replace(/[\[\].,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase();
+    return title_a > title_b ? 1 : -1;
+}
